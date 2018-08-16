76b64caaca7766184613bcd8ab22e63fdd504bc8
Ryan Hass <rhass@users.noreply.github.com>
2018-08-16 16:49:05 -0700

    Bump metadata version.
    Just a version bump so that anything riding in master is on a newer
    version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Ryan Hass'
 maintainer_email  'ryan@invalidchecksum.net'
 license           'MIT'
 description       'Installs Radarr.'
-version           '1.0.3'
+version           '1.0.4'
 issues_url        'https://github.com/rhass/radarr/issues'
 source_url        'https://github.com/rhass/radarr'
 
